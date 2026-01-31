a219137c5928d3f41fa1724469dbcc6156dd1f0b
Pallavi Patel <pallavipatel1713@gmail.com>
2026-01-31 15:09:47 +0530

    feat: Add `https://wtf-admin-neon.vercel.app` to the list of allowed CORS origins.

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://wtf-admin-neon.vercel.app'
 ];
 
 app.use(cors({
